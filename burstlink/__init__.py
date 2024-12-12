dedc817143353f605177f937a1d59e974e176c3b
LiyingZhou <liyingzhou556@gmail.com>
2024-12-12 20:07:56 +0800

    Update __init__.py

--- burstlink/__init__.py
+++ burstlink/__init__.py
@@ -3,6 +3,6 @@ import warnings
 warnings.filterwarnings('ignore')
 
 from . import _utils
-from . import preprocessing
-from . import tools
-from . import plotting
+from . import preprocessing as pp
+from . import tools as tl
+from . import plotting as pl
