05dedb8a4a3eacbfae520683a5c5909d2f8dba3e
LiyingZhou <liyingzhou556@gmail.com>
2024-12-13 10:05:12 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
-from setuptools import setup, find_packages 
+from setuptools import setup, find_packages
+from pathlib import Path
+
+long_description = Path("README.md").read_text(encoding="utf-8")
 
 setup(
-    name="burstlink",
-    version="0.2.1", 
-    packages=find_packages(),
-    python_requires=">=3.9",
+    name="burstlink",                  
+    version="0.3.6",                   
+    packages=find_packages(),            
+    python_requires=">=3.9",            
+    long_description=long_description,
+    long_description_content_type="text/markdown",  
 )
