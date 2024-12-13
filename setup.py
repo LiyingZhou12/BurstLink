759a5fc2033e8703c7dd0d6a7829d9f4e5758dba
LiyingZhou <liyingzhou556@gmail.com>
2024-12-13 09:19:07 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages 
 
 setup(
     name="burstlink",
     version="0.2.1", 
+    packages=find_packages(),
+    python_requires=">=3.9",
 )
