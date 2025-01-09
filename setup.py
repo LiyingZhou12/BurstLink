9fd4a3cf23dc5dc9fe1914a230db4afcc1bfb946
LiyingZhou <liyingzhou556@gmail.com>
2025-01-09 14:06:29 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from pathlib import Path
 
 long_description = Path("README.md").read_text(encoding="utf-8")
 
+def parse_requirements(filename):
+    with open(filename, "r") as file:
+        return [line.strip() for line in file if line and not line.startswith("#")]
+
 setup(
     name="burstlink",                  
     version="1.0.0",   
     description="A user-friendly package for analyzing gene interactions and transcriptional bursting.",                
     packages=find_packages(),            
     python_requires=">=3.8",   
-    install_requires=[
-        "statsmodels>=0.12.0",
-        "numpy>=1.21.0",
-        "pandas>=1.3.0",
-    ],
+    install_requires=parse_requirements("requirements.txt"),
     long_description=long_description,
     long_description_content_type="text/markdown",  
 )
