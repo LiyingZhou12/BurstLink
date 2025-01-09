4d940da6c5a5737514cc6f83bcec0725e14045b9
LiyingZhou <liyingzhou556@gmail.com>
2025-01-09 20:08:33 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,6 @@ release = '1.0.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx_mdinclude',
     'sphinx_rtd_theme',
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
