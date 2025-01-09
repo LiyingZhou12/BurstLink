68a0037ff27a659f2c61f7288893971b2cf4e49a
LiyingZhou <liyingzhou556@gmail.com>
2025-01-09 13:37:39 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ import sys
 from pathlib import Path
 
 HERE = Path(__file__).parent
-sys.path[:0] = [str(HERE.parent)]
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
