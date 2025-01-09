1a59b1c010062fe08a7e036c5785411328c826cc
LiyingZhou <liyingzhou556@gmail.com>
2025-01-09 20:14:10 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,87 +1,50 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 import os
 import sys
 from pathlib import Path
 
 HERE = Path(__file__).parent
-sys.path[:0] = [str(HERE.parent)]
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+PROJECT_ROOT = HERE.parent
+sys.path.insert(0, str(PROJECT_ROOT))
 
 project = 'BurstLink'
 copyright = '2025, LiyingZhou'
 author = 'LiyingZhou'
 release = '1.0.0'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
-    'sphinx_rtd_theme',
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
     'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon', 
     'sphinx.ext.mathjax',
-    'sphinx_autodoc_typehints',
-    'sphinx_rtd_size',
-    'nbsphinx',
-    'sphinx_gallery.load_style',
-    'myst_nb'
+    'myst_nb',
 ]
-myst_enable_extensions = [
-    'html_admonition',
-    'colon_fence',
-    
-]
-nbsphinx_allow_errors = True
-nbsphinx_execute = 'auto'
+autosummary_generate = True
+autodoc_mock_imports = ["rpy2", "numpy", "pandas"] 
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'private-members': False,
+    'special-members': '__init__',
+    'show-inheritance': True,
+    'source': True, 
+}
 nb_execution_mode = "off"
-sphinx_rtd_size_width = "75%"
-
 source_suffix = {
     '.rst': 'restructuredtext',
     '.ipynb': 'myst-nb',
-    '.myst': 'myst-nb',
-    '.md': 'markdown',
 }
-
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
 html_theme = "furo"
 html_static_path = ["_static"]
-html_css_files = [
-    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css",
-    "css/override.css",
-]
-
-html_show_sphinx = False
-html_show_sourcelink = False
+html_css_files = ["css/override.css"]
+html_logo = "_static/image/logo.png"
 html_theme_options = {
     "sidebar_hide_name": True,
     "light_css_variables": {
         "color-brand-primary": "#357473",
         "color-brand-content": "#357473",
-        "admonition-font-size": "var(--font-size-normal)",
-        "admonition-title-font-size": "var(--font-size-normal)",
     },
-    "footer_icons": [
-        {
-            "name": "GitHub",
-            "url": "https://github.com/LiyingZhou12/BurstLink",
-            "html": "",
-            "class": "fab fa-github",
-        },
-    ],
 }
-html_logo = "_static/image/logo.png"
