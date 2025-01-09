88540f50edea3f1bd73b454ad5dc3ffbd5305b10
LiyingZhou <liyingzhou556@gmail.com>
2025-01-09 22:41:03 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -42,6 +42,7 @@ exclude_patterns = [
     '_build/jupyter_execute/**',
     'Thumbs.db',
     '.DS_Store', 
+    'api/index.rst',
 ]
 
 # -- Options for HTML output -------------------------------------------------
