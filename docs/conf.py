4fa47b0302ff5fa8d947eb03dd82fd47493ab43b
LiyingZhou <liyingzhou556@gmail.com>
2025-01-09 15:28:22 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ extensions = [
     'sphinx.ext.napoleon', 
 ]
 autosummary_generate = True
-autodoc_mock_imports = ["BurstLink"]
+autodoc_mock_imports = ["burstlink"]
 autodoc_default_options = {
     'members': True,
     'undoc-members': True,
