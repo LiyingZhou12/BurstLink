8ecc1e31b5c6ecc961b1ed5dac4defa28a0f44a2
LiyingZhou <liyingzhou556@gmail.com>
2025-01-09 10:17:56 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,8 @@ import sys
 from pathlib import Path
 
 HERE = Path(__file__).parent
-sys.path[:0] = [str(HERE.parent)]
+# sys.path[:0] = [str(HERE.parent)]
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -16,7 +17,7 @@ sys.path[:0] = [str(HERE.parent)]
 project = 'BurstLink'
 copyright = '2024, LiyingZhou'
 author = 'LiyingZhou'
-release = '0.0.2'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
