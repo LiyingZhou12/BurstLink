ac14bef988af3b2645bd80f0bf7c0e0813994288
LiyingZhou <liyingzhou556@gmail.com>
2025-01-10 22:58:10 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath('../'))
 project = 'BurstLink'
 copyright = '2025, LiyingZhou'
 author = 'LiyingZhou'
-release = '1.0.3'
+release = '1.0.5'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
