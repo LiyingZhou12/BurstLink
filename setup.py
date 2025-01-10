70d09c98781a87ed83ff14ccc82cd0355fae1eb9
LiyingZhou <liyingzhou556@gmail.com>
2025-01-10 22:52:05 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,25 +1,19 @@
 from setuptools import setup, find_packages
 from pathlib import Path
-import os
 
-on_rtd = os.environ.get("READTHEDOCS") == "True"
 long_description = Path("README.md").read_text(encoding="utf-8")
-
-def parse_requirements(filename):
-    with open(filename, "r") as file:
-        return [line.strip() for line in file if line and not line.startswith("#")]
-
-install_requires = parse_requirements("requirements.txt")
-if not on_rtd:
-    install_requires.append("rpy2==3.5.16") 
 
 setup(
     name="burstlink",                  
-    version="1.0.3",   
+    version="1.0.5",   
     description="A user-friendly package for analyzing gene interactions and transcriptional bursting.",                
     packages=find_packages(),            
     python_requires=">=3.8",   
-    install_requires=install_requires, 
+    install_requires=[
+        "statsmodels>=0.12.0",
+        "numpy>=1.21.0",
+        "pandas>=1.3.0",
+    ],
     long_description=long_description,
     long_description_content_type="text/markdown",  
 )
