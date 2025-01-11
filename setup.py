9a96e933a1bffa58d09f9cf47f390e79de59c19f
LiyingZhou <liyingzhou556@gmail.com>
2025-01-11 09:26:39 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = Path("README.md").read_text(encoding="utf-8")
 
 setup(
     name="burstlink",                  
-    version="1.1.1",   
+    version="1.1.2",   
     description="A user-friendly package for analyzing gene interactions and transcriptional bursting.",                
     packages=find_packages(),            
     python_requires=">=3.8",   
